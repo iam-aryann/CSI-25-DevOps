9403658c819a8ba7a681ed4a10c8d762f2793a1e
Aryan Choudhary <215512154+iam-aryann@users.noreply.github.com>
2025-07-23 16:13:51 +0530

    Create PharmaWebAppApplicationsTests.java

---
+++ Project: WebApp CICD using GitHub Actions/src/test/java/PharmaWebAppApplicationsTests.java
@@ -0,0 +1,11 @@
+package com.example.pharmawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaWebAppApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
