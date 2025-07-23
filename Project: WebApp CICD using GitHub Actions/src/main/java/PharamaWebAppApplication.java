d9b88de7d7e000cafaba2bada493d5749ab26761
Aryan Choudhary <215512154+iam-aryann@users.noreply.github.com>
2025-07-23 16:09:03 +0530

    Create PharamaWebAppApplication.java

---
+++ Project: WebApp CICD using GitHub Actions/src/main/java/PharamaWebAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.pharmawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaWebAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaWebAppApplication.class, args);
+    }
+}
